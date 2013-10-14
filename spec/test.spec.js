306190907657023c0a7ed36acd7334096fe3e9eb
Szymon WYgnański <s@finalclass.net>
2013-10-14 13:02:47 +0200

    adding forceSet method

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -2,53 +2,63 @@ var dobj = require('../index.js');
 
 describe('dobj', function () {
 
-    var obj;
+  var obj;
 
-    beforeEach(function () {
-        obj = {
-            a: {
-                b: {
-                    c: 'test'
-                }
-            }
-        };
-    });
+  beforeEach(function () {
+    obj = {
+      a: {
+        b: {
+          c: 'test'
+        }
+      }
+    };
+  });
 
-    it('can get', function () {
-        expect(dobj(obj).get('a.b.c')).toBe('test');
-    });
+  it('can get', function () {
+    expect(dobj(obj).get('a.b.c')).toBe('test');
+  });
 
-    it('can set', function () {
-        dobj(obj).set('a.b', {d: 'test'});
-        expect(dobj(obj).get('a.b.d')).toBe('test');
-    });
+  it('can set', function () {
+    dobj(obj).set('a.b', {d: 'test'});
+    expect(dobj(obj).get('a.b.d')).toBe('test');
+  });
 
-    it('can delete', function () {
-        dobj(obj).del('a.b.c');
-        expect(dobj(obj).get('a.b.c')).not.toBeDefined();
-    });
+  it('can delete', function () {
+    dobj(obj).del('a.b.c');
+    expect(dobj(obj).get('a.b.c')).not.toBeDefined();
+  });
 
-    it('can shallow get', function () {
-        expect(dobj(obj).get('a')).toBe(obj.a);
-    });
+  it('can shallow get', function () {
+    expect(dobj(obj).get('a')).toBe(obj.a);
+  });
 
-    it('can shallow set', function () {
-        dobj(obj).set('a', 'ok');
-        expect(dobj(obj).get('a')).toBe('ok');
-    });
+  it('can shallow set', function () {
+    dobj(obj).set('a', 'ok');
+    expect(dobj(obj).get('a')).toBe('ok');
+  });
 
-    it('can shallowe del', function () {
-        dobj(obj).del('a');
-        expect(dobj(obj).get('a')).not.toBeDefined();
-    });
+  it('can shallowe del', function () {
+    dobj(obj).del('a');
+    expect(dobj(obj).get('a')).not.toBeDefined();
+  });
 
-    it('has chaining api', function () {
-        expect(function () {
-            dobj(obj)
-                .set('a.b.c', 'ok')
-                .del('a.b')
-                .get('a');
-        }).not.toThrow();
-    });
+  it('has chaining api', function () {
+    expect(function () {
+      dobj(obj)
+        .set('a.b.c', 'ok')
+        .del('a.b')
+        .get('a');
+    }).not.toThrow();
+  });
+
+  it('can force set', function () {
+    var obj2 = {};
+    
+    expect(function () {
+      dobj(obj2).forceSet('a.b.c', 'ok');
+    }).not.toThrow();
+
+    expect(obj2.a.b.c).toBe('ok');
+  });
 
 });
